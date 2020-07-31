da980f77b0c0e6c27bcc1ee936a2cde34ea7838f
Ire Olaniyan <ireolaniyan7@gmail.com>
2020-07-31 01:53:51 +0100

    Feat
    - Add repositories

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repository/ScheduleRepository.java
@@ -0,0 +1,16 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import com.udacity.jdnd.course3.critter.data.Employee;
+import com.udacity.jdnd.course3.critter.data.Pet;
+import com.udacity.jdnd.course3.critter.data.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+    List<Schedule> findByPets(Pet pet);
+    List<Schedule> findByEmployee(Employee employee);
+    List<Schedule> findByPetsIn(List<Pet> pets);
+}
