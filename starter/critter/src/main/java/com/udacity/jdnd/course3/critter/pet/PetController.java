ca26030c887a414ba8babd7309e1565337d65a8f
Ire Olaniyan <ireolaniyan7@gmail.com>
2020-07-31 01:54:58 +0100

    Feat
    - implement pet controller methods

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
@@ -1,8 +1,14 @@
 package com.udacity.jdnd.course3.critter.pet;
 
+import com.udacity.jdnd.course3.critter.data.Pet;
+import com.udacity.jdnd.course3.critter.service.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Handles web requests related to Pets.
@@ -11,23 +17,53 @@ import java.util.List;
 @RequestMapping("/pet")
 public class PetController {
 
+    @Autowired
+    PetService petService;
+
+    // Convert Entity to DTO
+    private PetDTO getPetsDTO(Pet pet) {
+        PetDTO petDTO = new PetDTO();
+        petDTO.setId(pet.getId());
+        petDTO.setBirthDate(pet.getBirthDate());
+        petDTO.setName(pet.getName());
+        petDTO.setNotes(pet.getNotes());
+        petDTO.setOwnerId(pet.getCustomer().getId());
+        petDTO.setType(pet.getType());
+        return  petDTO;
+    }
+
     @PostMapping
     public PetDTO savePet(@RequestBody PetDTO petDTO) {
-        throw new UnsupportedOperationException();
+        Pet pet = new Pet();
+        pet.setType(petDTO.getType());
+        pet.setName(petDTO.getName());
+        pet.setBirthDate(petDTO.getBirthDate());
+        pet.setNotes(petDTO.getNotes());
+
+        return getPetsDTO(petService.savePet(pet, petDTO.getOwnerId()));
     }
 
     @GetMapping("/{petId}")
     public PetDTO getPet(@PathVariable long petId) {
-        throw new UnsupportedOperationException();
+        Pet pet = petService.getPetById(petId);
+        return getPetsDTO(pet);
     }
 
     @GetMapping
     public List<PetDTO> getPets(){
-        throw new UnsupportedOperationException();
+        List<Pet> pets = petService.getAllPets();
+        return pets.stream().map(this::getPetsDTO).collect(Collectors.toList());
     }
 
     @GetMapping("/owner/{ownerId}")
     public List<PetDTO> getPetsByOwner(@PathVariable long ownerId) {
-        throw new UnsupportedOperationException();
+        List<Pet> pets = null;
+        try {
+            pets = petService.getPetsByCustomerId(ownerId);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Owner pet does not exist with id " + ownerId, e);
+        }
+
+        return pets.stream().map(this::getPetsDTO).collect(Collectors.toList());
     }
 }
