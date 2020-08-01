066d0d9634f696f9f99c588e49d138ae21fdf838
Ire Olaniyan <ireolaniyan7@gmail.com>
2020-08-01 16:38:16 +0100

    Fix
    - put error messages in schedule controller

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
@@ -5,7 +5,9 @@ import com.udacity.jdnd.course3.critter.data.Pet;
 import com.udacity.jdnd.course3.critter.data.Schedule;
 import com.udacity.jdnd.course3.critter.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,8 +31,14 @@ public class ScheduleController {
 
     @PostMapping
     public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
-        Schedule schedule = new Schedule(scheduleDTO.getDate(), scheduleDTO.getActivities());
-        return convertScheduleToScheduleDTO(scheduleService.saveSchedule(schedule, scheduleDTO.getEmployeeIds(), scheduleDTO.getPetIds()));
+        Schedule schedule = new Schedule(scheduleDTO.getDate(), scheduleDTO.getActivities());;
+        ScheduleDTO convertedSchedule;
+        try {
+            convertedSchedule = convertScheduleToScheduleDTO(scheduleService.saveSchedule(schedule, scheduleDTO.getEmployeeIds(), scheduleDTO.getPetIds()));
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Schedule could not be saved", exception);
+        }
+        return convertedSchedule;
     }
 
     @GetMapping
@@ -41,19 +49,34 @@ public class ScheduleController {
 
     @GetMapping("/pet/{petId}")
     public List<ScheduleDTO> getScheduleForPet(@PathVariable long petId) {
-        List<Schedule> schedules = scheduleService.getPetSchedule(petId);
+        List<Schedule> schedules;
+        try {
+            schedules = scheduleService.getPetSchedule(petId);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Pet schedule with id: " + petId + " not found", exception);
+        }
         return schedules.stream().map(this::convertScheduleToScheduleDTO).collect(Collectors.toList());
     }
 
     @GetMapping("/employee/{employeeId}")
     public List<ScheduleDTO> getScheduleForEmployee(@PathVariable long employeeId) {
-        List<Schedule> schedules = scheduleService.getEmployeeSchedule(employeeId);
+        List<Schedule> schedules;
+        try {
+            schedules = scheduleService.getEmployeeSchedule(employeeId);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Employee schedule with employee id: " + employeeId + " not found", exception);
+        }
         return schedules.stream().map(this::convertScheduleToScheduleDTO).collect(Collectors.toList());
     }
 
     @GetMapping("/customer/{customerId}")
     public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
-        List<Schedule> schedules = scheduleService.getCustomerSchedule(customerId);
+        List<Schedule> schedules;
+        try {
+            schedules = scheduleService.getCustomerSchedule(customerId);
+        } catch (Exception exception) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Schedule with owner id " + customerId + " not found", exception);
+        }
         return schedules.stream().map(this::convertScheduleToScheduleDTO).collect(Collectors.toList());
     }
 }
